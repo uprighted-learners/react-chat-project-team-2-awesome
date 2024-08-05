7b8f1fae05300ad2e851aa43aed1c0b404c78748
CodeBelow89 <ririck101@gmail.com>
2024-08-05 19:56:29 -0400

    Message

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const user = require("../../../../6-databases/complete_monogose/models/user");
+
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
