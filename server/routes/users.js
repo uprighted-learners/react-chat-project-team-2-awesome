7f1c6618d1e5d38821a87ab9fbf4b69b95e3f73a
ddsykes <ddsykes@hotmail.com>
2024-08-05 19:00:46 -0500

    Before Puling from Main

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,25 +3,24 @@ const router = express.Router();
 const User = require("../models/User");
 
 //Creating the user endpoints
+
 router.post("/", async (req, res) => {
-  router.post("/", async (req, res) => {
-    try {
-      const user = new User(re.body);
-      await user.save();
-      res.status(201).send(user);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
-  //Creating the users endpoint
-  router.post("/login", async (req, ees) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user && user.password === password) {
-      res.send(user);
-    } else {
-      res.status(400).send("Invalid User Login credentials");
-    }
-  });
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+//Creating the users endpoint
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && user.password === password) {
+    res.send(user);
+  } else {
+    res.status(400).send("Invalid User Login credentials");
+  }
 });
 module.exports = router;
