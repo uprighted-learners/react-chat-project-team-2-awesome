0ba0344c823f022fd1e6727a0f9a729c17b68786
ddsykes <ddsykes@hotmail.com>
2024-08-08 20:09:16 -0500

    Adding the updated project

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require ('bcrypt');
+const JWT = require("jsonwebtoken");
 
 //Creating the user endpoints
 
 router.post("/", async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+ const saltRounds = parseInt(process.env.SALT);
+  bcrypt.hash(req.body.password, saltRounds, async(err, hash) =>{
+    try {
+      const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hash
+      });
+      await user.save();
+      let token = JWT.sign({id:user._id}, process.env.JWTKEY, {expiresIn:"1 day"} )
+      res.status(201).json({
+        User: user,
+        Token: token,
+      });
+
+    } catch (error) {
+      console.log(error)
+      res.status(400).send(error);
+    }
+  })
+  
 });
 //Creating the users endpoint
 router.post("/login", async (req, res) => {
