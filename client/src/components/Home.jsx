c0d2a5a8c92b0260850d0fb4ef03b1bf253dfaa4
ddsykes <ddsykes@hotmail.com>
2024-08-12 18:56:13 -0500

    Adding the changes for the project.

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import Auth from './Auth'
+
+const Home = () => {
+ const [token, setToken] = useState(localStorage.getItem)('token');   
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Home
