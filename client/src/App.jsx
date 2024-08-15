fade2c0994709205a1ff61d69f59ce4327153cca
CodeBelow89 <ririck101@gmail.com>
2024-08-15 19:30:54 -0400

    message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState, } from 'react'; //useState intializes `token` state with value from `localStorage`, UseEffect updates `localStorage` with the new value
-import Auth from './components/Auth'; //user authentication
-import Room from './components/Room'; // for interacting with selected room
-
+import React, { useEffect, useState } from "react"; //useState intializes `token` state with value from `localStorage`, UseEffect updates `localStorage` with the new value
+import Auth from "./components/Auth"; //user authentication
+import Room from "./components/Room"; // for interacting with selected room
 
 //functional react component to render different components based on the state
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem("token") || ''); //state to store the authentication token retrieved from `localStorage`
+  const [token, setToken] = useState(localStorage.getItem("token") || ""); //state to store the authentication token retrieved from `localStorage`
   const [selectedRoom, setSelectedRoom] = useState(null); //function to update selected room
 
-//Updates local storage when token changes
+  //Updates local storage when token changes
   useEffect(() => {
     if (token) {
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
     }
   }, [token]);
 
-//Conditional rendering 
+  //Conditional rendering
   return (
     <div className="App">
       {token ? (
@@ -23,7 +22,6 @@ const App = () => {
       ) : (
         <>
           <Room setSelectedRoom={setSelectedRoom} />
-          {selectedRoom && <Room room={selectedRoom} />}
         </>
       )}
     </div>
